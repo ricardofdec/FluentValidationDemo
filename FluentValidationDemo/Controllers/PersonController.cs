0020179cad74bcb03a86d9ce0dfa1d7fb6ae1366
Ricardo Castro <ricardofc.ti@outlook.com>
2021-04-22 22:43:51 -0300

    Add project files.

---
+++ FluentValidationDemo/Controllers/PersonController.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidationDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FluentValidationDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("Post")]
+        public Person Post(Person model)
+        {
+            return model;
+        }
+    }
+}
---
+++ FluentValidationDemo/Models/Person.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using System;
+
+namespace FluentValidationDemo.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int Identity { get; set; }
+    }
+
+    public class PersonValidator : AbstractValidator<Person>
+    {
+        public PersonValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty();
+
+            RuleFor(x => x.Identity)
+                .Must(x => x != 0)
+                .WithMessage("cannot be zero");
+
+            RuleFor(x => x.BirthDate)
+                .Must(ValidateAgeOfMajority)
+                .WithMessage("Minors are not allowed.");
+        }
+
+        private bool ValidateAgeOfMajority(DateTime birthDate) => Convert.ToDateTime(birthDate).AddYears(18) < DateTime.Now ? true : false;
+
+    }
+}
